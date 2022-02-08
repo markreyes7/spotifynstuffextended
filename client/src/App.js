4f9901f0197219a3aaa9ded4a0d77c9a0b075d8b
Mark <mark.reyes7777@gmail.com>
2022-02-08 01:12:33 -0800

    Added a userinterface and user interaction to show my current favorite artists and add to a playlist that exists in my spotify account.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,20 @@ import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
 import  Modal  from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Navbar from 'react-bootstrap/Navbar'
 
 
 function App() {
 
-  const [data, setData] = useState([{}]);
-  const [track, setTrackToDelete] = useState(0)
+  const [data, setData] = useState([]);
+  const [track, setTrackToDelete] = useState(0);
+  const [artists, setArtists] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+ 
 
   async function reload(){
    await fetch("/playlist").then(res =>res.json())
@@ -19,6 +27,8 @@ function App() {
    })
   }
 
+  
+
   useEffect(() => {
     fetch("/playlist").then(res => res.json())
     .then(data => {
@@ -27,9 +37,127 @@ function App() {
     })
   },[] )
 
+  useEffect(() =>{
+    fetch("/long_term_artists").then(res => res.json())
+    .then(artists => {
+      setArtists(artists)
+      console.log(artists.artists_list[0])
+      console.log(artists.artists_images)
+    })
+  },[])
+
   return (
-    <div>
-      <Modal show={true}>
+    <main>
+      <Container>
+        <Navbar bg='light'>
+          <Navbar.Brand>SpotifyNStuff</Navbar.Brand>
+          <Button onClick={() =>{setShowModal(!showModal)}}>Click to display random playlist</Button>
+        </Navbar>
+      </Container>
+      <section class="py-5 text-center container">
+        <div class="row py-lg-5">
+          <div class="col-lg-6 col-md-8 mx-auto">
+            <h1 class="fw-light">Album example</h1>
+            <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+          <p> <Button onClick={ async () =>{
+                const response = await fetch("/long_term_artists").then(res => res.json()
+                .then(artists => {
+                  setArtists(artists)
+                }
+                
+                ))
+                if(response.ok){
+                  console.log("yay")
+                }
+              }}>Favorites Over The Years</Button>
+              <Button onClick={ async () =>{
+                const response = await fetch("/medium_term_artists").then(res => res.json()
+                .then(artists => {
+                  setArtists(artists)
+                }
+                
+                ))
+                if(response.ok){
+                  console.log("yay")
+                }
+              }}>Favorites From Last Year</Button>
+              <Button onClick={
+                async () =>{
+                  const response = await fetch("/short_term_artists").then(res => res.json()
+                  .then(artists => {
+                    setArtists(artists)
+                  }
+                  
+                  ))
+                  if(response.ok){
+                    console.log("yay")
+                  }
+                }
+              }>The last 4 weeks</Button>
+           </p>
+         </div>
+          </div>
+      </section>
+    {((typeof artists.artists_list === 'undefined') ? (<p>Loading</p>):(
+      <Container>
+        <Row>
+      
+        <Col>
+          <Card style={{width: '18rem'}}>
+            <Card.Img src={artists.artists_images[0]}></Card.Img>
+              <Card.Body>
+                <Card.Title>{artists.artists_list[0]}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+        <Col>
+          <Card style={{width: '18rem'}}>
+            <Card.Img src={artists.artists_images[1]}></Card.Img>
+              <Card.Body>
+                <Card.Title>{artists.artists_list[1]}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      
+      </Row>
+      <Row>
+      <Col>
+          <Card style={{width: '18rem'}}>
+            <Card.Img src={artists.artists_images[2]}></Card.Img>
+              <Card.Body>
+                <Card.Title>{artists.artists_list[2]}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+        <Col>
+          <Card style={{width: '18rem'}}>
+            <Card.Img src={artists.artists_images[3]}></Card.Img>
+              <Card.Body>
+                <Card.Title>{artists.artists_list[3]}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+        <Col>
+          <Card style={{width: '18rem'}}>
+            <Card.Img src={artists.artists_images[4]}></Card.Img>
+              <Card.Body>
+                <Card.Title>{artists.artists_list[4]}</Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      </Container>
+    )
+    )}
+
+      <Modal show={showModal} onHide={async () => {
+        await fetch("/close_connection_cursor")
+        
+        setShowModal(!showModal);
+      }}>
         <Modal.Header closeButton>
           <Modal.Title>Remove a song</Modal.Title>
         </Modal.Header>
@@ -39,11 +167,13 @@ function App() {
             (data.members.map((member, i) => (<ListGroupItem key={i}>{member}</ListGroupItem>)))}
           </ListGroup>
         </Modal.Body>
+        
         <Modal.Footer>
           <Form>
             <Form.Group >
               <Form.Label>Song To Delete</Form.Label>
               <Form.Control placeholder='Input digits only.' type='number' value={track} onChange={e => setTrackToDelete(e.target.value)}></Form.Control>
+              <Container>
               <Button onClick={async () => {
                 const songToDelete = {track}
                 console.log(track)
@@ -61,18 +191,28 @@ function App() {
                   reload();
                 }
               }}>Delete this track</Button>
+              </Container>
+              <Container>
+                <Button onClick={ async () =>{
+                 const r = await fetch("/addsong")
+                if(r.ok){
+                  reload();
+                }
+                }}>Click me to add a random song</Button>
+              </Container>
+              
             </Form.Group>
           </Form>
         </Modal.Footer>
       </Modal>
-    </div>
+    </main>
     
-    // <div>
-    //   {(typeof data.members === 'undefined') ? 
-    //   (<p>Loading....</p>) : (
-    //    data.members.map((member, i) =>(<p key={i}>{member}</p>)))
-    //   }
-    // </div>
+   /* <div className='container-fluid'>
+       <ListGroup>
+         {(typeof artists.artists_list === 'undefined') ? (<Spinner animation='border' role="status">Loading</Spinner>) :
+          (artists.artists_list.map((artist, i) =>(<ListGroupItem key={i}>{artist}</ListGroupItem>) ))}
+       </ListGroup>
+      </div> */
   );
 }
 
--- server.py
+++ server.py
@@ -1,14 +1,19 @@
 from crypt import methods
+import re
 from urllib import response
+from itsdangerous import json
 import psycopg2
 import os
 import spotipy
 from flask import Flask, jsonify, request
 from spotipy.oauth2  import SpotifyOAuth
+import random
 
 app = Flask(__name__)
 
-scope = 'playlist-read-private playlist-read-collaborative'
+scope = 'playlist-read-private playlist-read-collaborative user-top-read'
+
+#TODO:FIX close_connection bugs and fix open connection bug.
 
 connection = psycopg2.connect(user=os.environ['USER'],
                               password=os.environ['PASSWORD'],
@@ -20,6 +25,8 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIP
                                                     client_secret=os.environ['SPOTIPY_CLIENT_SECRET'],
                                                     redirect_uri='https://google.com/',
                                                     scope=scope))
+
+
 
 @app.route("/playlist")
 def playlist():
@@ -47,8 +54,129 @@ def removesong():
     spotify.playlist_remove_specific_occurrences_of_items(os.environ['PLAYLIST_ID'],
                                                           [{"uri": song_to_remove, "positions": [int(song) - 1]}])
 
-    
     return "Done", 201
+
+
+
+@app.route("/addsong", methods=['POST', 'GET'])
+def addsong():
+    cursor = connection.cursor()
+    def get_random_id_from_db():
+    
+        sql_SELECT_QUERY = "SELECT artist_id FROM artist ORDER BY RANDOM() LIMIT 1;"
+
+        cursor.execute(sql_SELECT_QUERY)
+        x = cursor.fetchone()[0]
+        return x
+
+
+    def get_random_album_id(artist_id):
+        all_albums = spotify.artist_albums(artist_id, limit=25)
+        length_of_all_albums = len(all_albums["items"])
+        position = all_albums['items'][random.randint(0, length_of_all_albums - 1)]  # generates a random album
+        album_id = position["id"]
+        return album_id
+
+
+    def get_random_song(album_id):
+        current_tracks = spotify.album_tracks(album_id)
+        total_tracks = len(current_tracks["items"])
+        pos = current_tracks['items'][random.randint(0, total_tracks - 1)]
+        song_name = pos['name']
+        song_uri = pos['uri']
+        return song_name, song_uri,
+
+
+
+    song_import = False
+    id_from_db = get_random_id_from_db()
+    album_id = get_random_album_id(id_from_db)
+    song_tuple = get_random_song(album_id)
+    song_name = song_tuple[0]
+    song_id = song_tuple[1]
+
+    print(song_name)
+    cursor.execute("""SELECT song_name FROM artist WHERE artist_id = %s """, (id_from_db,))
+    for record in cursor:
+        if song_name == record[0]:
+            print("Matching songs")
+            addsong()
+            break
+        else:
+            print("no match. \n Adding to the playlist")
+            song_import = True
+        print(record[0] + " is our current song")
+    
+    if song_import:
+        spotify.playlist_add_items(os.environ['PLAYLIST_ID'], [song_id])
+    return jsonify({"song_name": song_name })
+
+@app.route("/close_connection_cursor")
+def close_connection_cursor():
+    connection.close()
+    return "Done", 201
+
+    
+@app.route("/long_term_artists")
+def long_term_artists():
+    spotify_request_artist = spotify.current_user_top_artists(limit=5, offset=0, time_range='long_term')
+    artists = spotify_request_artist['items']
+    artist_list = []
+    image_list = []
+    images_to_return =[]
+    for count, value in enumerate(artists):
+        artist_list.append(artists[count]['name'])
+
+    for count, value in enumerate(artists):
+        image_list.append(artists[count]['images'])
+    
+    for list in image_list:
+         images_to_return.append(list[0]['url'])
+           
+
+    return jsonify({"artists_list": artist_list,
+                    "artists_images": images_to_return})
+
+@app.route("/medium_term_artists")
+def medium_term_artists():
+    spotify_request_artist = spotify.current_user_top_artists(limit=5, offset=0, time_range='medium_term')
+    artists = spotify_request_artist['items']
+    artist_list = []
+    image_list = []
+    images_to_return =[]
+    for count, value in enumerate(artists):
+        artist_list.append(artists[count]['name'])
+
+    for count, value in enumerate(artists):
+        image_list.append(artists[count]['images'])
+    
+    for list in image_list:
+         images_to_return.append(list[0]['url'])
+           
+
+    return jsonify({"artists_list": artist_list,
+                    "artists_images": images_to_return})
+
+
+@app.route("/short_term_artists")
+def short_term_artists():
+    spotify_request_artist = spotify.current_user_top_artists(limit=5, offset=0, time_range='short_term')
+    artists = spotify_request_artist['items']
+    artist_list = []
+    image_list = []
+    images_to_return =[]
+    for count, value in enumerate(artists):
+        artist_list.append(artists[count]['name'])
+
+    for count, value in enumerate(artists):
+        image_list.append(artists[count]['images'])
+    
+    for list in image_list:
+         images_to_return.append(list[0]['url'])
+           
+
+    return jsonify({"artists_list": artist_list,
+                    "artists_images": images_to_return})
 
 
 if __name__ == "__main__":
