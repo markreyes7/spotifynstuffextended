34ce43b76cc2aaf7ec5ac2267df1aea5c466176e
Mark <mark.reyes7777@gmail.com>
2022-02-11 10:58:53 -0800

    added some css styling and some inline css in jsx

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Navbar from 'react-bootstrap/Navbar'
+import Alert from 'react-bootstrap/Alert'
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
   const [artists, setArtists] = useState([{}]);
   const [showModal, setShowModal] = useState(false);
   const [count, setCount] = useState(0);
+  const [showAlert, setShowAlert] = useState(false)
  
   async function reload(){
    await fetch("/playlist").then(res =>res.json())
@@ -56,15 +58,16 @@ function App() {
           <Navbar.Brand color='white'>SpotifyNStuff</Navbar.Brand>
           </Container>
          
-          <Button size='sm' variant='light' onClick={() =>{setShowModal(!showModal)}}>Click to display random playlist</Button>
+          <Button size='sm' variant='outline-light' onClick={() =>{setShowModal(!showModal)}}>Click to display random playlist</Button>
         </Navbar>
       
       <section class="py-5 text-center container">
         <div class="row py-lg-5">
           <div class="col-lg-6 col-md-8 mx-auto">
             <h1 class="fw-light">Album example</h1>
-            <p class="lead text-muted">Based on your spotify informationa and data, enter a number to get some of my favorite artists of all time, from the last year, or last couple of weeks. Click the top button to get my current random playlist and feel free to add a track or delete a random track. These tracks came from songs that do not exist in my personal database.</p>
-          <Row> <Button variant='dark' onClick={
+            <p class="lead p-color">Based on your spotify informationa and data, enter a number to get some of my favorite artists of all time, from the last year, or last couple of weeks. Click the top button to get my current random playlist and feel free to add a track or delete a random track. These tracks came from songs that do not exist in my personal database.</p>
+          <Row> 
+            <Button variant='dark' onClick={
                 async () =>{
                   const num = {count}
                   const response = await fetch("/long_term_artists",{
@@ -82,7 +85,7 @@ function App() {
                   }
                 }
               }>Favorites Over The Years</Button>
-              <Button variant='dark' onClick={
+              <Button style={{marginTop: "10px"}} variant='dark' onClick={
                 async () =>{
                   const num = {count}
                   const response = await fetch("/medium_term_artists",{
@@ -100,7 +103,7 @@ function App() {
                   }
                 }
               }>Favorites From Last Year</Button>
-              <Button variant='dark' onClick={
+              <Button style={{marginTop: "10px"}} variant='dark' onClick={
                 async () =>{
                   const num = {count}
                   const response = await fetch("/short_term_artists",{
@@ -127,7 +130,7 @@ function App() {
          </div>
           </div>
       </section>
-      <div className='album py-5 bg-light'>
+      <div className='album py-5'>
       <Container>
       {((typeof artists.artists_list === 'undefined') ? (<p>Loading</p>):(
         <Row>
@@ -135,8 +138,11 @@ function App() {
           <Col style={{marginBottom: "10px "}} >
             <Card text='white' bg='dark' style={{width: '16rem'}} >
             <Card.Body>
-            <Card.Img src={artists.artists_images[i]}></Card.Img>
-                <Card.Title>{artist}</Card.Title>
+              <Card.Img src={artists.artists_images[i]}></Card.Img>
+              <Card.Title>{artist}</Card.Title>
+              <Container>
+              
+              </Container>
             </Card.Body>
             </Card>
           </Col>) }
@@ -160,6 +166,9 @@ function App() {
             {(typeof data.members === 'undefined') ? (<p>Loading....</p>) :
             (data.members.map((member, i) => (<ListGroupItem as='li' key={i}>{member + " : " + data.artist_names[i]} </ListGroupItem>)) )}
           </ListGroup>
+          <Container>
+          <iframe  title='playlist' src="https://open.spotify.com/embed/playlist/0vWGvkCnbashwFP0gNCNN8?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" sandbox='allow-same-origin allow-scripts allow-forms'></iframe>
+          </Container>
         </Modal.Body>
         
         <Modal.Footer>
@@ -180,12 +189,11 @@ function App() {
                   body: JSON.stringify(songToDelete)
                 })
               
-                if (response.ok){
-                  console.log("loading the current playlist")
-                  reload();
+                if (!response.ok){
+                  setShowAlert(!showAlert)
                 }
                 else{
-                  console.log("bad boy")
+                  reload()
                 }
               }}>Delete this track</Button>
               </Container>
@@ -200,6 +208,12 @@ function App() {
               
             </Form.Group>
           </Form>
+          <Container>
+            <Alert show={showAlert} variant='danger'>
+              <Alert.Heading>Lost Connection to Databse</Alert.Heading>
+              <p>Please reload page</p>
+            </Alert>
+          </Container>
         </Modal.Footer>
       </Modal>
       </Container>
