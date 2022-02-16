5e9e3a94d8fc8979e3f10432b03064a27b1b3cfe
Mark <mark.reyes7777@gmail.com>
2022-02-15 23:50:17 -0800

    added some changes to the server and some styling on the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import ListGroup from 'react-bootstrap/ListGroup'
 import ListGroupItem from 'react-bootstrap/esm/ListGroupItem';
-import  Modal  from 'react-bootstrap/Modal';
+import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
@@ -10,152 +10,166 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Navbar from 'react-bootstrap/Navbar'
 import Alert from 'react-bootstrap/Alert'
+import Badge from 'react-bootstrap/Badge'
+
+
 
 function App() {
 
   const [data, setData] = useState([]);
   const [track, setTrackToDelete] = useState(0);
   const [artists, setArtists] = useState([{}]);
+  const [info, setInfo] = useState([])
   const [showModal, setShowModal] = useState(false);
   const [count, setCount] = useState(0);
   const [showAlert, setShowAlert] = useState(false)
- 
-  async function reload(){
-   await fetch("/playlist").then(res =>res.json())
-   .then(data =>{
-     setData(data)
-     console.log(data);
-   })
+
+  async function reload() {
+    await fetch("/playlist").then(res => res.json())
+      .then(data => {
+        setData(data)
+        console.log(data);
+      })
   }
+
+
+  useEffect(() => {
+    fetch("/search_genre_of_week").then(res => res.json())
+      .then(info => {
+        setInfo(info)
+        console.log(info);
+      })
+  }, [])
+
 
   useEffect(() => {
     fetch("/playlist").then(res => res.json())
-    .then(data => {
-      setData(data)
-      console.log(data)
-    })
-  },[] )
+      .then(data => {
+        setData(data)
+        console.log(data)
+      })
+  }, [])
 
-  useEffect(() =>{
-    const num = {count}
+  useEffect(() => {
+    const num = { count }
     fetch("/long_term_artists", {
       method: "POST",
-      headers:{"Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(num)
     }).then(res => res.json())
-    .then(artists => {
-      setArtists(artists)
-      console.log(artists.artists_list[0])
-      console.log(artists.artists_images)
-    })
-  },[])
+      .then(artists => {
+        setArtists(artists)
+        console.log(artists.artists_list[0])
+        console.log(artists.artists_images)
+      })
+  }, [])
 
   return (
     <Container className='custom-sizing' fluid>
-      
-        <Navbar bg='dark' variant='dark'>
-          <Container>
+
+      <Navbar bg='dark' variant='dark'>
+        <Container>
           <Navbar.Brand color='white'>SpotifyNStuff</Navbar.Brand>
-          </Container>
-         
-          <Button size='sm' variant='outline-light' onClick={() =>{setShowModal(!showModal)}}>Click to display random playlist</Button>
-        </Navbar>
-      
+        </Container>
+
+        <Button size='sm' variant='outline-light' onClick={() => { setShowModal(!showModal) }}>Click to display random playlist</Button>
+      </Navbar>
+
       <section class="py-5 text-center container">
         <div class="row py-lg-5">
           <div class="col-lg-6 col-md-8 mx-auto">
             <h1 class="fw-light">Album example</h1>
             <p class="lead p-color">Based on your spotify informationa and data, enter a number to get some of my favorite artists of all time, from the last year, or last couple of weeks. Click the top button to get my current random playlist and feel free to add a track or delete a random track. These tracks came from songs that do not exist in my personal database.</p>
-          <Row> 
-            <Button variant='dark' onClick={
-                async () =>{
-                  const num = {count}
-                  const response = await fetch("/long_term_artists",{
+            <Row>
+              <Button variant='dark' onClick={
+                async () => {
+                  const num = { count }
+                  const response = await fetch("/long_term_artists", {
                     method: "POST",
-                    headers:{"Content-Type": "application/json"},
+                    headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(num)
-                  } ).then(res => res.json()
-                  .then(artists => {
-                    setArtists(artists)
-                  }
-                  
-                  ))
-                  if(response.ok){
+                  }).then(res => res.json()
+                    .then(artists => {
+                      setArtists(artists)
+                    }
+
+                    ))
+                  if (response.ok) {
                     console.log("yay")
                   }
                 }
               }>Favorites Over The Years</Button>
-              <Button style={{marginTop: "10px"}} variant='dark' onClick={
-                async () =>{
-                  const num = {count}
-                  const response = await fetch("/medium_term_artists",{
+              <Button style={{ marginTop: "10px" }} variant='dark' onClick={
+                async () => {
+                  const num = { count }
+                  const response = await fetch("/medium_term_artists", {
                     method: "POST",
-                    headers:{"Content-Type": "application/json"},
+                    headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(num)
-                  } ).then(res => res.json()
-                  .then(artists => {
-                    setArtists(artists)
-                  }
-                  
-                  ))
-                  if(response.ok){
-                    
+                  }).then(res => res.json()
+                    .then(artists => {
+                      setArtists(artists)
+                    }
+
+                    ))
+                  if (response.ok) {
+
                   }
                 }
               }>Favorites From Last Year</Button>
-              <Button style={{marginTop: "10px"}} variant='dark' onClick={
-                async () =>{
-                  const num = {count}
-                  const response = await fetch("/short_term_artists",{
+              <Button style={{ marginTop: "10px" }} variant='dark' onClick={
+                async () => {
+                  const num = { count }
+                  const response = await fetch("/short_term_artists", {
                     method: "POST",
-                    headers:{"Content-Type": "application/json"},
+                    headers: { "Content-Type": "application/json" },
                     body: JSON.stringify(num)
-                  } ).then(res => res.json()
-                  .then(artists => {
-                    setArtists(artists)
-                  }
-                  
-                  ))
-                  if(response.ok){
+                  }).then(res => res.json()
+                    .then(artists => {
+                      setArtists(artists)
+                    }
+
+                    ))
+                  if (response.ok) {
                     console.log("")
                   }
                 }
               }>The last 4 weeks</Button>
-              <Form style={{marginTop: '10px'}}>
+              <Form style={{ marginTop: '10px' }}>
                 <Form.Control placeholder='Enter a number to get a range of favorite artists. i.e(5)' type='number' value={count} onChange={e => setCount(e.target.value)}>
 
                 </Form.Control>
               </Form>
-           </Row>
-         </div>
+            </Row>
           </div>
+        </div>
       </section>
       <div className='album py-5'>
-      <Container>
-      {((typeof artists.artists_list === 'undefined') ? (<p>Loading</p>):(
-        <Row>
-          {artists.artists_list.map((artist, i) => 
-          <Col style={{marginBottom: "10px "}} >
-            <Card text='white' bg='dark' style={{width: '16rem'}} >
-            <Card.Body>
-              <Card.Img src={artists.artists_images[i]}></Card.Img>
-              <Card.Title>{artist}</Card.Title>
-              <Container>
-              
-              </Container>
-            </Card.Body>
-            </Card>
-          </Col>) }
-        </Row>
-      )
-    )}
-      </Container>
+        <Container>
+          {((typeof artists.artists_list === 'undefined') ? (<p>Loading</p>) : (
+            <Row>
+              {artists.artists_list.map((artist, i) =>
+                <Col style={{ marginBottom: "10px " }} >
+                  <Card text='white' bg='dark' style={{ width: '16rem' }} >
+                    <Card.Body>
+                      <Card.Img src={artists.artists_images[i]}></Card.Img>
+                      <Card.Title>{artist}</Card.Title>
+                      <Container>
+
+                      </Container>
+                    </Card.Body>
+                  </Card>
+                </Col>)}
+            </Row>
+          )
+          )}
+        </Container>
       </div>
-   
+
 
       <Modal show={showModal} onHide={async () => {
         await fetch("/close_connection_cursor")
-        
+
         setShowModal(!showModal);
       }}>
         <Modal.Header closeButton>
@@ -164,48 +178,48 @@ function App() {
         <Modal.Body>
           <ListGroup as='ol' numbered>
             {(typeof data.members === 'undefined') ? (<p>Loading....</p>) :
-            (data.members.map((member, i) => (<ListGroupItem as='li' key={i}>{member + " : " + data.artist_names[i]} </ListGroupItem>)) )}
+              (data.members.map((member, i) => (<ListGroupItem as='li' key={i}>{member + " : " + data.artist_names[i]} </ListGroupItem>)))}
           </ListGroup>
           <Container>
-          <iframe  title='playlist' src="https://open.spotify.com/embed/playlist/0vWGvkCnbashwFP0gNCNN8?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" sandbox='allow-same-origin allow-scripts allow-forms'></iframe>
+            <iframe title='playlist' src="https://open.spotify.com/embed/playlist/0vWGvkCnbashwFP0gNCNN8?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" sandbox='allow-same-origin allow-scripts allow-forms'></iframe>
           </Container>
         </Modal.Body>
-        
+
         <Modal.Footer>
           <Form>
             <Form.Group >
               <Form.Label>Song To Delete</Form.Label>
               <Form.Control placeholder='Input digits only.' type='number' value={track} onChange={e => setTrackToDelete(e.target.value)}></Form.Control>
               <Container>
-              <Button onClick={async () => {
-                const songToDelete = {track}
-                console.log(track)
-                const response = await fetch("/removesong", {
-                  method: "POST",
-                  headers:{
-                    "Content-Type": "application/json"
-                  },
-                  
-                  body: JSON.stringify(songToDelete)
-                })
-              
-                if (!response.ok){
-                  setShowAlert(!showAlert)
-                }
-                else{
-                  reload()
-                }
-              }}>Delete this track</Button>
+                <Button onClick={async () => {
+                  const songToDelete = { track }
+                  console.log(track)
+                  const response = await fetch("/removesong", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json"
+                    },
+
+                    body: JSON.stringify(songToDelete)
+                  })
+
+                  if (!response.ok) {
+                    setShowAlert(!showAlert)
+                  }
+                  else {
+                    reload()
+                  }
+                }}>Delete this track</Button>
               </Container>
               <Container>
-                <Button onClick={ async () =>{
-                 const r = await fetch("/addsong")
-                if(r.ok){
-                  reload();
-                }
+                <Button onClick={async () => {
+                  const r = await fetch("/addsong")
+                  if (r.ok) {
+                    reload();
+                  }
                 }}>Click me to add a random song</Button>
               </Container>
-              
+
             </Form.Group>
           </Form>
           <Container>
@@ -216,7 +230,34 @@ function App() {
           </Container>
         </Modal.Footer>
       </Modal>
+      <Container>
+        <Container>
+          <h3 style={{display: "flex", justifyContent: "center" }}>Top Genres Played Last Week</h3>
+        </Container>
+
       </Container>
+      <div>
+        {(typeof info === 'undefined') ? (<p>true</p>) :
+          <ListGroup variant="flush">
+            {Object.keys(info).map((key, i) => (
+              <ListGroup.Item className="d-flex justify-content-between align-items-start" variant='dark' key={i}>
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">
+                    {key}
+                  </div>
+
+                </div>
+                <Badge variant="primary" pill>
+                  {info[key]}
+                </Badge>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        }
+
+      </div>
+
+    </Container>
   );
 }
 
--- server.py
+++ server.py
@@ -5,16 +5,18 @@ from itsdangerous import json
 import psycopg2
 import os
 import spotipy
+
 from flask import Flask, jsonify, request
-from spotipy.oauth2  import SpotifyOAuth
+import json
+from spotipy.oauth2 import SpotifyOAuth
 import random
 import datetime
+from collections import Counter
+
 app = Flask(__name__)
 
 
-
 scope = 'playlist-read-private playlist-read-collaborative user-top-read'
-
 
 
 connection = psycopg2.connect(user=os.environ['USER'],
@@ -29,7 +31,6 @@ spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ['SPOTIP
                                                     scope=scope))
 
 
-
 @app.route("/playlist")
 def playlist():
     json_boy = spotify.playlist_items(os.environ['PLAYLIST_ID'])
@@ -41,7 +42,7 @@ def playlist():
     names = []
     for track in playlist:
         songs.append(track["track"]["name"])
-    
+
     for artist in playlist:
         names.append(artist["track"]["artists"][0]["name"])
     print("yessir")
@@ -58,7 +59,6 @@ def removesong():
         genres = curr_artist["genres"]
         return genres
 
-
     def import_deleted_to_db(track):
         postgres_insert_query = """INSERT INTO artist (artist_name, song_name, make_date ,listened, genre, artist_id) VALUES (
                 %s, %s, %s, %s, %s, %s) """
@@ -70,51 +70,49 @@ def removesong():
         genres = get_genres(artist_id)
         song_name = track['name']
         print("song was " + song_name)
-        record_to_insert = (artist_name, song_name, song_listen_time, True, genres, artist_id)
+        record_to_insert = (artist_name, song_name,
+                            song_listen_time, True, genres, artist_id)
         cursor.execute(postgres_insert_query, record_to_insert)
         connection.commit()
         print("The artist was inserted")
-        
-
 
     try:
         song_to_delete = request.json
-        song = song_to_delete['track']  
+        song = song_to_delete['track']
         curr_playlist = spotify.playlist_items(os.environ['PLAYLIST_ID'])
         items = curr_playlist['items']
         track = items[int(song) - 1]['track']
         song_to_remove = track['uri']
 
         spotify.playlist_remove_specific_occurrences_of_items(os.environ['PLAYLIST_ID'],
-                                                          [{"uri": song_to_remove, "positions": [int(song) - 1]}])
+                                                              [{"uri": song_to_remove, "positions": [int(song) - 1]}])
         import_deleted_to_db(track)
         cursor.close()
     except:
-        return "bad", 400
+        return "Reload page", 500
 
     return "Done", 201
-
 
 
 @app.route("/addsong", methods=['POST', 'GET'])
 def addsong():
     cursor = connection.cursor()
+
     def get_random_id_from_db():
-    
+
         sql_SELECT_QUERY = "SELECT artist_id FROM artist ORDER BY RANDOM() LIMIT 1;"
 
         cursor.execute(sql_SELECT_QUERY)
         x = cursor.fetchone()[0]
         return x
 
-
     def get_random_album_id(artist_id):
         all_albums = spotify.artist_albums(artist_id, limit=25)
         length_of_all_albums = len(all_albums["items"])
-        position = all_albums['items'][random.randint(0, length_of_all_albums - 1)]  # generates a random album
+        position = all_albums['items'][random.randint(
+            0, length_of_all_albums - 1)]  # generates a random album
         album_id = position["id"]
         return album_id
-
 
     def get_random_song(album_id):
         current_tracks = spotify.album_tracks(album_id)
@@ -124,8 +122,6 @@ def addsong():
         song_uri = pos['uri']
         return song_name, song_uri,
 
-
-
     song_import = False
     id_from_db = get_random_id_from_db()
     album_id = get_random_album_id(id_from_db)
@@ -134,7 +130,8 @@ def addsong():
     song_id = song_tuple[1]
 
     print(song_name)
-    cursor.execute("""SELECT song_name FROM artist WHERE artist_id = %s """, (id_from_db,))
+    cursor.execute(
+        """SELECT song_name FROM artist WHERE artist_id = %s """, (id_from_db,))
     for record in cursor:
         if song_name == record[0]:
             print("Matching songs")
@@ -144,67 +141,69 @@ def addsong():
             print("no match. \n Adding to the playlist")
             song_import = True
         print(record[0] + " is our current song")
-    
+
     if song_import:
         spotify.playlist_add_items(os.environ['PLAYLIST_ID'], [song_id])
-    return jsonify({"song_name": song_name })
+    return jsonify({"song_name": song_name})
+
 
 @app.route("/close_connection_cursor")
 def close_connection_cursor():
     connection.close()
     return "Done", 201
 
-    
+
 @app.route("/long_term_artists", methods=['POST'])
 def long_term_artists():
 
     request_from_client = request.json
     artist_count = request_from_client['count']
-    
-    if artist_count == 0:
-        artist_count =8
 
-    spotify_request_artist = spotify.current_user_top_artists(limit=artist_count, offset=0, time_range='long_term')
+    if artist_count == 0:
+        artist_count = 8
+
+    spotify_request_artist = spotify.current_user_top_artists(
+        limit=artist_count, offset=0, time_range='long_term')
     artists = spotify_request_artist['items']
     artist_list = []
     image_list = []
-    images_to_return =[]
+    images_to_return = []
     for count, value in enumerate(artists):
         artist_list.append(artists[count]['name'])
 
     for count, value in enumerate(artists):
         image_list.append(artists[count]['images'])
-    
+
     for list in image_list:
-         images_to_return.append(list[0]['url'])
-           
+        images_to_return.append(list[0]['url'])
 
     return jsonify({"artists_list": artist_list,
                     "artists_images": images_to_return})
+
 
 @app.route("/medium_term_artists", methods=['POST'])
 def medium_term_artists():
 
     request_from_client = request.json
     artist_count = request_from_client['count']
-    
-    if artist_count == 0:
-        artist_count =8
 
-    spotify_request_artist = spotify.current_user_top_artists(limit=artist_count, offset=0, time_range='medium_term')
+    if artist_count == 0:
+        artist_count = 8
+
+    spotify_request_artist = spotify.current_user_top_artists(
+        limit=artist_count, offset=0, time_range='medium_term')
     artists = spotify_request_artist['items']
     artist_list = []
     image_list = []
-    images_to_return =[]
+    images_to_return = []
     for count, value in enumerate(artists):
         artist_list.append(artists[count]['name'])
 
     for count, value in enumerate(artists):
         image_list.append(artists[count]['images'])
-    
+
     for list in image_list:
-         images_to_return.append(list[0]['url'])
-           
+        images_to_return.append(list[0]['url'])
 
     return jsonify({"artists_list": artist_list,
                     "artists_images": images_to_return})
@@ -215,29 +214,84 @@ def short_term_artists():
     request_from_client = request.json
     artist_count = request_from_client['count']
     if artist_count == 0:
-        artist_count =8
+        artist_count = 8
 
-    spotify_request_artist = spotify.current_user_top_artists(limit=artist_count, offset=0, time_range='short_term')
+    spotify_request_artist = spotify.current_user_top_artists(
+        limit=artist_count, offset=0, time_range='short_term')
     artists = spotify_request_artist['items']
     artist_list = []
     image_list = []
-    images_to_return =[]
+    images_to_return = []
     for count, value in enumerate(artists):
         artist_list.append(artists[count]['name'])
 
     for count, value in enumerate(artists):
         image_list.append(artists[count]['images'])
-    
+
     for list in image_list:
-         images_to_return.append(list[0]['url'])
-           
+        images_to_return.append(list[0]['url'])
 
     return jsonify({"artists_list": artist_list,
                     "artists_images": images_to_return})
 
 
+# @app.route("/search_db_artists_of_prev_week")
+# def search_db_artists_of_prev_week():
+#     postgres_insert_query = "SELECT artist_name FROM artist WHERE make_date  BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW(" \
+#                             "))::INTEGER-7 AND NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER "
+#     cursor = connection.cursor()
+#     cursor.execute(postgres_insert_query)
+#     value = cursor.fetchall()
+#     array = []
+#     counter = 0
+#     for i in value:
+#         array.append(i[0])
+#         array.sort()
+#         print(array)
+
+#     while counter < len(array) - 1:
+#         curr = array[counter]
+#         new = ""
+#         inner_count = 0
+
+#         while curr != new:
+#             if curr == array[counter + 1]:
+#                 print("dupy")
+#                 counter += 1
+#                 inner_count += 1
+#                 artists_of_the_week[curr] = inner_count
+#             else:
+#                 artists_of_the_week[curr] = inner_count + 1
+#                 new = curr
+#                 print("no")
+#                 counter += 1
+#                 inner_count = 0
+#     with open("sample.json", "w") as outfile:
+#         json.dump(artists_of_the_week, outfile)
+#         print("done")
+#     return "Done", 201
+
+@app.route("/search_genre_of_week")
+def search_genre_of_week():
+    postgres_insert_query = "SELECT genre FROM artist WHERE make_date  BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW(" \
+                            "))::INTEGER-7 AND NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER"
+    cnt = Counter()
+    cursor = connection.cursor()
+    cursor.execute(postgres_insert_query)
+    value = cursor.fetchall()
+    array = []
+
+    for i in value:
+        array.append(i[0])    #get the tuple out and make a 2D array
+
+    for index in array:
+        for genre in index:
+            cnt[genre] += 1
+
+    new_dict = dict(cnt.most_common())
+    json_object = json.dumps(new_dict, indent=4)
+    print(json_object)
+    return json_object
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-   
